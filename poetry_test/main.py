da1c3fff9aceb7f5743e5af300d0f15bf715baf9
renmurakami <autoeyy@gmail.com>
2022-06-06 21:03:27 +0900

    pillow test

--- poetry_test/main.py
+++ poetry_test/main.py
@@ -1,3 +1,8 @@
+from statistics import quantiles
+from PIL import Image
+
+img_path = "./poetry_test/E7AB8BE88AB1E99FBF.jpg"
+
 print("hello world")
 
 def math(x):
@@ -5,3 +10,19 @@ def math(x):
     print(doubleX)
 
 math(3)
+
+img = Image.open(img_path)
+
+img_w = img.width
+img_h = img.height
+
+res_w = 340
+
+res_raito = 340 / img_w
+
+res_size = (res_w, round(img_h * res_raito))
+
+print(res_size)
+
+res_img = img.resize(res_size)
+res_img.save("dist/output.jpg", quantiles=90)
